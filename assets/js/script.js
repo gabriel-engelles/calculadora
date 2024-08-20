af91b14b96d0045eca018f3e9dceada6d66b4a4f
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-08-20 18:52:30 +0100

    Adicionado eventos de onclick no index.html. Adicionadas funções para gerenciar operadores e cálculos. Implementado timeout para desligamento automático após 4 minutos de inatividade. Corrigida a lógica de adição de números ao display, incluindo a prevenção de múltiplos pontos decimais e corte de resultados longos para até 13 caracteres. Arredondamento de resultados para duas casas decimais ao exibir valores decimais.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,30 @@ function iniciarTimeout() {
 }
 iniciarTimeout()
 
-// Função chamada ao clicar em "Clear", resetando o display
 let oC = document.querySelector('#OC')
 oC.addEventListener('click', onClear)
-
+// Função chamada ao clicar em "Clear", resetando o display
 function onClear() {
-    let display = addNumber()
-    if(display === "" || display >= 0 ){
+    let display = addNumero()
+    if(display === "" || display >= 0 || display.indexOf(".") !== -1){
         document.querySelector('#display').value = 0
+        operador = ""
+        valorAnterior = undefined
+        resultado = undefined
+        valorAtual = undefined
     }
 }
 
 // Função para adicionar números ao display
-function addNumber(num) {
+function addNumero(num) {
     clearTimeout(timeout);// Mantém a calculadora "ligada"
     iniciarTimeout();
-    let displayValue = document.querySelector('#display').value 
+    let displayValue = document.querySelector('#display').value
+    
+    if(num == "." && displayValue.includes(".")){
+        return
+    }
+
     if(displayValue == 0) { 
         document.querySelector('#display').value = num
     }else if (displayValue.length < 13){
@@ -32,6 +40,75 @@ function addNumber(num) {
     }
     return displayValue
 }
+
+let valorAnterior
+let operador 
+let valorAtual
+let resultado
+
+// Função para adicionar/alterar operador aritmético, adicionar valor ou zerar variaveis
+function opAritmetico(op) {
+    if(valorAnterior === undefined){
+        valorAnterior = Number(document.querySelector('#display').value)
+        console.log("anterior: " + valorAnterior)
+    } else {
+        resultado = Number(document.querySelector('#display').value)
+        valorAtual = undefined
+        console.log("anterior(resultado): " + resultado)
+    }
+    operador = op
+    document.querySelector('#display').value = ""
+}
+
+// Função para calcular valores
+function calcular() {
+    if(valorAnterior === undefined) {
+        return
+    } else if(valorAtual === undefined) {
+        valorAtual = Number(document.querySelector('#display').value)
+    }
+
+    
+    switch (operador) {
+        case "+":
+            resultado = (resultado === undefined) ? (isNaN(valorAnterior) ? 0 : valorAnterior) + valorAtual : Number(resultado) + valorAtual
+            break;
+        case "-":
+            resultado = (resultado === undefined) ? (isNaN(valorAnterior) ? 0 : valorAnterior) - valorAtual : Number(resultado) - valorAtual
+            break;
+        case "*":
+            resultado = (resultado === undefined) ? (isNaN(valorAnterior) ? 0 : valorAnterior) * valorAtual : Number(resultado) * valorAtual
+            break;
+        case "/":
+            resultado = (resultado === undefined) ? (isNaN(valorAnterior) ? 0 : valorAnterior) / valorAtual : Number(resultado) / valorAtual
+            break;
+    
+        default:
+            break;
+    }
+
+    
+    resultado = resultado.toString()
+
+    if (resultado.length > 13) {
+        resultado = resultado.slice(0, 13)
+    }
+    
+    if (resultado.includes(".")) {
+        console.log(resultado)
+        resultado = parseFloat(resultado).toFixed(2)
+        console.log(resultado)
+    }
+    document.querySelector('#display').value = resultado
+}
+
+
+
+
+
+
+
+
 
 
 
